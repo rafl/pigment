3827f9e29ac1ce52e4f6fbea4c091d59ff4618e8
Florian Ragwitz <rafl@debian.org>
2009-01-26 09:07:26 +0100

    Add a hypothetical example to see what the api might look like.

---
+++ examples/text.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Pigment;
+
+my $viewport = Pigment::ViewportFactory->make('opengl');
+$viewport->set_title('Text');
+
+my $txt = Pigment::Text->new;
+$txt->set_markup(<<'EOM')
+<b>PgmText</b> is a drawable displaying a <u>text</u> with support for
+multiple lines, <i><b>markups</b> and several</i> properties.
+EOM
+
+$txt->set_font_height(0.12);
+$txt->set_size(1, 1);
+$txt->set_position(1.5, 1, 0);
+$txt->fg_color(240, 240, 240, 255);
+$txt->bg_color(20, 20, 20, 255);
+$txt->show;
+
+my $canvas = Pigment::Canvas->new;
+$viewport->set_canvas($canvas);
+$canvas->add('middle', $txt);
+
+$viewport->signal_connect('delete-event' => sub {
+    Pigment->main_quit;
+});
+
+$viewport->show;
+
+Pigment->main;
