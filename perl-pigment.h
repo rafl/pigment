2f4a0a78e841545dcdf7757b96bf29c87eefc9de
Florian Ragwitz <rafl@debian.org>
2009-01-26 10:01:00 +0100

    Helper macro to assert PgmErrors.

--- perl-pigment.h
+++ perl-pigment.h
@@ -1,3 +1,8 @@
 #include <gtk2perl.h>
 #include <pgm/pgm.h>
 #include "pigment-autogen.h"
+
+#define PERL_PIGMENT_ASSERT_ERROR(error) \
+	if (error != PGM_ERROR_OK) { \
+		croak ("Generic pigment error"); \
+	}
